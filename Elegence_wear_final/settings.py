bac6cca953b08ed15c453c910ce2e829e8e9d28f
FARAZ QUAZI <141524293+quazifaraz508@users.noreply.github.com>
2025-08-16 18:45:26 +0530

    Update settings.py

--- Elegence_wear_final/settings.py
+++ Elegence_wear_final/settings.py
@@ -211,4 +211,11 @@ SOCIALACCOUNT_PROVIDERS = {
     }
 }
 
+# Force HTTPS in allauth redirects
+ACCOUNT_DEFAULT_HTTP_PROTOCOL = "https"
+
+# Ensure CSRF and cookies are secure
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+
 SECURE_SSL_REDIRECT = True
